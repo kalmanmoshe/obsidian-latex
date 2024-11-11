c69531a0816c4002f31c0db710977fefd8945925
kalmanmoshe <moshe196713@gmail.com>
2024-11-11 20:45:19 +0200

    Auto-commit: 2024-11-11 20:45:19

--- src/tikzjax/tikzjax.ts
+++ src/tikzjax/tikzjax.ts
@@ -760,6 +760,7 @@ export class Coordinate {
     }
 
     toString() {
+        console.log(this)
         switch (this.mode) {
             case "coordinate":
                 return `\\coor{${this.axis.toString()}}{${this.coordinateName || ""}}{${this.label || ""}}{}`;
