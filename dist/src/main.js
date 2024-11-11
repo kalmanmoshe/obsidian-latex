9ccd95c5e4eea11713c85e8bdc115144ecbf03e3
kalmanmoshe <moshe196713@gmail.com>
2024-11-11 21:00:40 +0200

    Auto-commit: 2024-11-11 21:00:40

--- dist/src/main.js
+++ dist/src/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c994d58c012ea0432e4f3860448ebe7466e8998e4ce57d20197ecfc0b3abf38e
-size 69241
+oid sha256:6552dd526dd7f4dd2c3c66e0bbbc69a40622c7b05911c9feb520559645cae4bf
+size 69231
