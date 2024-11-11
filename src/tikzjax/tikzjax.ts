4d8f50105550045d41a2b8b0eeda7e91eb317a0e
kalmanmoshe <moshe196713@gmail.com>
2024-11-11 19:29:09 +0200

    Auto-commit: 2024-11-11 19:29:09

--- src/tikzjax/tikzjax.ts
+++ src/tikzjax/tikzjax.ts
@@ -942,7 +942,7 @@ class FormatTikzjax {
         tikzSource = tikzSource.replaceAll(remove, "");let lines = tikzSource.split("\n");
         lines = lines.map(line => line.trim());
         lines = lines.filter(line => line);
-        lines.join('\n');
+        lines.join('\n').replace(/(?<=[^\w]) | (?=[^\w])/g, "").replace(/(?<!\\)%.*$/gm, "");;
         
     }
 
