94ff9e02fbef672a9a2ad1753152ed67622a1174
kalmanmoshe <moshe196713@gmail.com>
2024-11-11 20:51:25 +0200

    Auto-commit: 2024-11-11 20:51:25

--- src/main.ts
+++ src/main.ts
@@ -275,7 +275,7 @@ class VecProcessor {
     const ancer=new Axis();
     ancer.addCartesian(0,0);
 
-    const c1=new Coordinate("node-inline");
+    const c1=new Coordinate("node-inline",);
     c1.addAxis(this.axis.cartesianX,0)
 
     this.graph=new FormatTikzjax([
