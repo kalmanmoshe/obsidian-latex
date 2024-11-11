fe971544eadd4a1581e0982cc35105cde02d7412
kalmanmoshe <moshe196713@gmail.com>
2024-11-11 20:09:56 +0200

    Auto-commit: 2024-11-11 20:09:56

--- src/tikzjax/tikzjax.ts
+++ src/tikzjax/tikzjax.ts
@@ -520,7 +520,7 @@ export class Formatting{
         this.formattingSpecificToMode();
         this.interpretFormatting(formattingForInterpretation||"")
 
-        for(const)
+        for(const s og )
         this.rotate=toNumber(formatting?.rotate)??this.rotate;
         this.anchor=formatting?.anchor?.replace(/-\|/,"south")?.replace(/\|-/,"north")??this.anchor;
         
