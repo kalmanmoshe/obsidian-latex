f57c797fec9ae6980ccfac00f511c01d6cfa6639
kalmanmoshe <moshe196713@gmail.com>
2024-11-11 19:05:45 +0200

    Auto-commit: 2024-11-11 19:05:44

--- src/tikzjax/tikzjax.ts
+++ src/tikzjax/tikzjax.ts
@@ -934,7 +934,7 @@ class FormatTikzjax {
 	constructor(source: string|Tokens) {
         if(typeof source==="string")
 		this.source = source.replace(/(?<=[^\w]) | (?=[^\w])/g, "").replace(/(?<!\\)%.*$/gm, "");
-        else this.tokens
+        else this.tokens=source
         this.debugInfo+=this.source;
         this.tokenize();
         this.findMidpoint();
