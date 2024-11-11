15400f4d2cab4f6517b15646ecc540d1b5444c79
kalmanmoshe <moshe196713@gmail.com>
2024-11-11 20:03:52 +0200

    Auto-commit: 2024-11-11 20:03:52

--- src/main.ts
+++ src/main.ts
@@ -274,7 +274,7 @@ class VecProcessor {
     
     const ancer=new Axis().addCartesian(0,0);
     const c1=new Coordinate().addAxis(this.axis.cartesianX,0);
-
+    
     this.graph=new FormatTikzjax([
       new Formatting().quickAdd("globol",{color: "white",scale: "1",}),
       new Draw({formatting: {lineWidth: 1,draw: "yellow",arror: "-{Stealth}"},draw: [ancer,'--',c1,new Axis()]},undefined,"draw",),
