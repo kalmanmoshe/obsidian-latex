fcf6df4dc7f598523aef00610b360190888b01f4
kalmanmoshe <moshe196713@gmail.com>
2024-11-11 20:05:08 +0200

    Auto-commit: 2024-11-11 20:05:08

--- src/main.ts
+++ src/main.ts
@@ -282,7 +282,6 @@ class VecProcessor {
       //new Draw({formatting: {lineWidth: 1,draw: "yellow",arror: "-{Stealth}"},draw: [ancer,'--',new Coordinate(),new Axis()]},undefined,"draw",),
     ])
     this.vecInfo.addDebugInfo("this.graph", this.graph);
-    this.vecInfo.addDebugInfo("this.graph", this.graph.tostring());
     /* Generate LaTeX code for vector components and main vector
     const t = String.raw`
 
