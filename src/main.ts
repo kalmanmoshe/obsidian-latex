ac55402eed7cab4b26f3b11f212faba5d14ce4e1
kalmanmoshe <moshe196713@gmail.com>
2024-11-11 21:16:17 +0200

    Auto-commit: 2024-11-11 21:16:17

--- src/main.ts
+++ src/main.ts
@@ -297,7 +297,6 @@ class VecProcessor {
       % Angle Annotation
       %\ang{X}{anc}{vec}{}{${roundBySettings(vectorAngle)}$^{\circ}$}
     `.replace(/^\s+/gm, "");*/
-    new
     this.vecInfo.addDebugInfo("Scaling factor", scale);
   }
 }
