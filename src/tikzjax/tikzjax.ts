4894daaa9d5fa1aacfe7b9d265ab6ade465c6b1d
kalmanmoshe <moshe196713@gmail.com>
2024-11-11 19:58:33 +0200

    Auto-commit: 2024-11-11 19:58:33

--- src/tikzjax/tikzjax.ts
+++ src/tikzjax/tikzjax.ts
@@ -737,6 +737,7 @@ export class Coordinate {
     addInfo(match: {original?: string,coordinateName?: string,label?: string,formatting?: string}, mode: string,tokens?: FormatTikzjax,formatting?: object) {
         this.mode=mode;
         ([{original: this.original,coordinateName: this.coordinateName,label: this.label}]=[match])
+        
         if(this.original){
             this.axis=new Axis().universal(this.original,tokens);
         }
