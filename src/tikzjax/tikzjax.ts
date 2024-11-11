ea1e04b21a4b14c2e4fff6f6a5f87d75ea6aaeb2
kalmanmoshe <moshe196713@gmail.com>
2024-11-11 20:38:05 +0200

    Auto-commit: 2024-11-11 20:38:05

--- src/tikzjax/tikzjax.ts
+++ src/tikzjax/tikzjax.ts
@@ -803,7 +803,7 @@ export class Draw {
             this.coordinates = this.fillCoordinates(this.getSchematic(match.draw), tokens);
         }
         else{
-            this.coordinates=c
+            this.coordinates=this.createFromArray
         }
     }
 
