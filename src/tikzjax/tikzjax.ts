d082707c4502213f913ea5b8646576997a628b8d
kalmanmoshe <moshe196713@gmail.com>
2024-11-11 19:02:45 +0200

    Auto-commit: 2024-11-11 19:02:45

--- src/tikzjax/tikzjax.ts
+++ src/tikzjax/tikzjax.ts
@@ -252,6 +252,7 @@ export class Axis {
                 case (/[\d\w]+/).test(match):
                     if (tokens)
                     axis = tokens.findOriginalValue(match)?.axis;
+                else 
                     if (axis === undefined) {
                         throw new Error(`Couldn't find the coordinate ${match} from ${coordinate}`);
                     }
