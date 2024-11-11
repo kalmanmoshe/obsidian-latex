d8fba6c6efd15932a9834699d17d93e4373cd333
kalmanmoshe <moshe196713@gmail.com>
2024-11-11 19:51:05 +0200

    Auto-commit: 2024-11-11 19:51:05

--- src/main.ts
+++ src/main.ts
@@ -273,7 +273,7 @@ class VecProcessor {
     //const vectorAngle = getUsableDegrees(radiansToDegrees(Math.atan2(scaledY, scaledX)));
     
     const ancer=new Axis().addCartesian(0,0);
-    this.graph=new [
+    this.graph=new FormatTikzjax[
       new Formatting().quickAdd("globol",{color: "white",scale: "1",}),
       new Draw({formatting: {lineWidth: 1,draw: "yellow",arror: "-{Stealth}"},draw: [ancer,'--',new Coordinate(),new Axis()]},undefined,"draw",),
       new Draw({formatting: {lineWidth: 1,draw: "yellow",arror: "-{Stealth}"},draw: [ancer,'--',new Coordinate(),new Axis()]},undefined,"draw",),
@@ -528,8 +528,4 @@ class BinomInfoModel extends Modal {
       if (i > this.k) this.big += probability;
       if (i >= this.k) this.bigEqual += probability;
     }
-  }
-}
-
-
-
+  
