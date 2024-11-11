2d1ec96a1fbb111972bddec2a8f6cb625fd44f04
kalmanmoshe <moshe196713@gmail.com>
2024-11-11 19:25:26 +0200

    Auto-commit: 2024-11-11 19:25:26

--- src/tikzjax/tikzjax.ts
+++ src/tikzjax/tikzjax.ts
@@ -1065,7 +1065,7 @@ class FormatTikzjax {
     findOriginalValue(value: string) {
         const og = this.tokens.slice().reverse().find(
             (token: Token) =>
-                (token instanceof Axis) && token.coordinateName === value
+                (token instanceof Coordinate) && token.coordinateName === value
         );
         return og instanceof Coordinate ? og.clone() : undefined;
     }
