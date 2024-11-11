c0a3ad3518309bde6ee7e05e3316f872a67a2d57
kalmanmoshe <moshe196713@gmail.com>
2024-11-11 20:11:10 +0200

    Auto-commit: 2024-11-11 20:11:10

--- src/tikzjax/tikzjax.ts
+++ src/tikzjax/tikzjax.ts
@@ -522,7 +522,7 @@ export class Formatting{
 
         for (const [key, value] of Object.entries(this)) {
             if(typeof value === 'object'){
-                this.setProperty()
+                this.setProperty(key)
             }
             else if (value) {
                 string+=matchKeyWithValue(key as keyof Formatting)+(typeof value==="boolean"?'':value)+',';
