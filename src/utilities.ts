9d3973fe0eaef96015dcaa730c4c7a000699a341
Kalmanmoshe <moshe196713@gmail.com>
2024-12-12 15:04:15 +0200

    modified:   dist/src/imVeryLazy.js 	modified:   dist/src/main.js 	modified:   dist/src/mathEngine.js 	modified:   dist/src/mathUtilities.js 	modified:   dist/src/suggestor.js 	modified:   dist/src/temp.js 	modified:   dist/src/tikzjax/interpret/tokenizeTikzjax.js 	modified:   dist/src/tikzjax/myTikz.js 	modified:   dist/src/tikzjax/tikzCommands.js 	modified:   dist/src/tikzjax/tikzjax.js 	modified:   dist/src/utilities.js 	modified:   dist/src/utils/symbols.js 	modified:   dist/src/utils/tokenUtensils.js 	modified:   main.js 	modified:   src/main.ts 	modified:   src/suggestor.ts 	modified:   src/tikzjax/interpret/tokenizeTikzjax.ts 	modified:   src/utilities.ts

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,8 +1,5 @@
-/**
- * Returns an array of built-in mathjs symbols including functions, constants, and physical constants
- * Also includes a prefix to indicate the type of symbol
- * @returns {string[]} Array of mathjs built-in symbols
- */
+
+
 export interface Latex{
 	value: string;
 
@@ -11,7 +8,7 @@ export interface Latex{
 export function getMathJsSymbols(): Array<Latex> {
 
 	const Latex=[
-		{value: 'aaaa'}
+		{value: 'babc'}
 	];
 
 	return Latex;
