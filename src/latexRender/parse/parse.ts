3b88a435db42286bbc6b9dbeecd02ca43b6ec4d0
kalmanmoshe <moshe196713@gmail.com>
2025-02-12 17:52:57 +0200

    modified:   data.json 	modified:   src/latexRender/parse/parse.ts 	modified:   src/latexRender/parse/typs/ast-types.ts

--- src/latexRender/parse/parse.ts
+++ src/latexRender/parse/parse.ts
@@ -1,6 +1,12 @@
+import { Root,String, Whitespace,Parbreak,Comment, Macro,Environment, Argument, DisplayMath, Group, InlineMath, Verb, VerbatimEnvironment } from './typs/ast-types';
+
 let parse: any, deleteComments: any,toString:any,createMatchers:any,parsePgfkeys:any,pgfkeysArgToObject:any;
 
+interface utilMacros{
+    LATEX_NEWCOMMAND: Set<string>;
+    XPARSE_NEWCOMMAND: Set<string>;
 
+}
 import('@unified-latex/unified-latex-util-to-string').then(module => {
 	toString = module.toString;
 });
@@ -26,8 +32,41 @@ import('@unified-latex/unified-latex-util-pgfkeys').then(module => {
 interface ast{
 
 }
+function migrate(ast: any) {
+    switch (ast.type) {
+        case "root":
+            return new Root(ast.content.map(migrate), ast._renderInfo, ast.position);
+        case "string":
+            return new String(ast.content.map(migrate), ast._renderInfo, ast.position);
+        case "whitespace":
+            return new Whitespace(ast._renderInfo, ast.position);
+        case "parbreak":
+            return new Parbreak(ast._renderInfo, ast.position);
+        case "comment":
+            return new Comment(ast.content.map(migrate), ast.sameline, ast.suffixParbreak, ast.leadingWhitespace, ast._renderInfo, ast.position);
+        case "Macro":
+            return new Macro(ast.name, ast.args.map(migrate), ast._renderInfo, ast.position);
+        case "Environment":
+            return new Environment(ast.name, ast.args.map(migrate), ast.content.map(migrate), ast._renderInfo, ast.position);
+        case "VerbatimEnvironment":
+            return new VerbatimEnvironment(ast.name, ast.args.map(migrate), ast.content.map(migrate), ast._renderInfo, ast.position);
+        case "DisplayMath":
+            return new DisplayMath(ast.content.map(migrate), ast._renderInfo, ast.position);
+        case "InlineMath":
+            return new InlineMath(ast.content.map(migrate), ast._renderInfo, ast.position);
+        case "Group":
+            return new Group(ast.content.map(migrate), ast._renderInfo, ast.position);
+        case "Argument":
+            return new Argument(ast.content.map(migrate), ast._renderInfo, ast.position);
+        case "Verb":
+            return new Verb(ast.content.map(migrate), ast._renderInfo, ast.position);
+        default:
+            throw new Error(`Unknown node type: ${ast.type}`);
+    }
+}
 
 export class LatexabstractSyntaxTree{
+    
     packages: Array<string>;
     libraries: Array<string>;
     ast: any;
@@ -43,12 +82,5 @@ export class LatexabstractSyntaxTree{
     usdPackages(){}
     usdLibraries(){}
     usdCommands(){}
-    usdEnvironments(){}
-    
-}
-
-class Environment{
-    
-    env:string;
-    //args: Array<Argument>;
+    usdEnvironments(){}   
 }
--- src/latexRender/parse/typs/ast-types.ts
+++ src/latexRender/parse/typs/ast-types.ts
@@ -7,92 +7,153 @@ export interface GenericNode {
     type: string;
     _renderInfo?: object;
 }
-
-// Abstract nodes
-interface BaseNode {
-    type: string;
-    _renderInfo?: (MacroInfo["renderInfo"] | EnvInfo["renderInfo"]) & {
+type _renderInfo=(MacroInfo["renderInfo"] | EnvInfo["renderInfo"]) & {
         defaultArg?: string;
-    } & Record<string, unknown>;
+    } & Record<string, unknown>
+class BaseNode {
+    type: string;
+    _renderInfo?: _renderInfo;
     position?: {
         start: { offset: number; line: number; column: number };
         end: { offset: number; line: number; column: number };
     };
+    constructor(type: string, renderInfo?: typeof this._renderInfo, position?: typeof this.position) {
+        this.type = type;
+        this._renderInfo = renderInfo;
+        this.position = position;
+    }
 }
 
-
-
-interface ContentNode extends BaseNode {
+export class ContentNode extends BaseNode {
     content: Node[];
+    constructor(type: string, content: Node[], renderInfo?: typeof BaseNode.prototype._renderInfo, position?: typeof BaseNode.prototype.position) {
+        super(type, renderInfo, position);
+        this.content = content;
+    }
 }
 
 // Actual nodes
-export interface Root extends ContentNode {
-    type: "root";
-}
-export interface String extends BaseNode {
-    type: "string";
-    content: string;
-}
-
-export interface Whitespace extends BaseNode {
-    type: "whitespace";
+export class Root extends ContentNode {
+    type: "root" = "root";
+    constructor(content: Node[], renderInfo?: typeof BaseNode.prototype._renderInfo, position?: typeof BaseNode.prototype.position) {
+        super("root", content, renderInfo, position);
+    }
 }
 
-export interface Parbreak extends BaseNode {
-    type: "parbreak";
+export class String extends ContentNode {
+    type: "string" = "string";
+    constructor(content: Node[], renderInfo?: typeof BaseNode.prototype._renderInfo, position?: typeof BaseNode.prototype.position) {
+        super("string", content, renderInfo, position);
+    }
 }
 
-export interface Comment extends BaseNode {
-    type: "comment";
+export class Whitespace extends BaseNode {
+    type: "whitespace" = "whitespace";
+    constructor(renderInfo?: typeof BaseNode.prototype._renderInfo, position?: typeof BaseNode.prototype.position) {
+        super("whitespace", renderInfo, position);
+    }
+}
+
+export class Parbreak extends BaseNode {
+    type: "parbreak" = "parbreak";
+    constructor(renderInfo?: typeof BaseNode.prototype._renderInfo, position?: typeof BaseNode.prototype.position) {
+        super("parbreak", renderInfo, position);
+    }
+}
+
+export class Comment extends BaseNode {
+    type: "comment" = "comment";
     content: string;
     sameline?: boolean;
     suffixParbreak?: boolean;
     leadingWhitespace?: boolean;
+    constructor(content: string, sameline?: boolean, suffixParbreak?: boolean, leadingWhitespace?: boolean, renderInfo?: typeof BaseNode.prototype._renderInfo, position?: typeof BaseNode.prototype.position) {
+        super("comment", renderInfo, position);
+        this.content = content;
+        this.sameline = sameline;
+        this.suffixParbreak = suffixParbreak;
+        this.leadingWhitespace = leadingWhitespace;
+    }
 }
 
-export interface Macro extends BaseNode {
-    type: "macro";
+export class Macro extends BaseNode {
+    type: "macro" = "macro";
     content: string;
     escapeToken?: string;
     args?: Argument[];
+    constructor(content: string, escapeToken?: string, args?: Argument[], renderInfo?: typeof BaseNode.prototype._renderInfo, position?: typeof BaseNode.prototype.position) {
+        super("macro", renderInfo, position);
+        this.content = content;
+        this.escapeToken = escapeToken;
+        this.args = args;
+    }
 }
 
-export interface Environment extends ContentNode {
+export class Environment extends ContentNode {
     type: "environment" | "mathenv";
     env: string;
     args?: Argument[];
+    constructor(type: "environment" | "mathenv", env: string, content: Node[], args?: Argument[], renderInfo?: typeof BaseNode.prototype._renderInfo, position?: typeof BaseNode.prototype.position) {
+        super(type, content, renderInfo, position);
+        this.env = env;
+        this.args = args;
+    }
 }
 
-export interface VerbatimEnvironment extends BaseNode {
-    type: "verbatim";
+export class VerbatimEnvironment extends BaseNode {
+    type: "verbatim" = "verbatim";
     env: string;
     content: string;
+    constructor(env: string, content: string, renderInfo?: typeof BaseNode.prototype._renderInfo, position?: typeof BaseNode.prototype.position) {
+        super("verbatim", renderInfo, position);
+        this.env = env;
+        this.content = content;
+    }
 }
 
-export interface DisplayMath extends ContentNode {
-    type: "displaymath";
+export class DisplayMath extends ContentNode {
+    type: "displaymath" = "displaymath";
+    constructor(content: Node[], renderInfo?: typeof BaseNode.prototype._renderInfo, position?: typeof BaseNode.prototype.position) {
+        super("displaymath", content, renderInfo, position);
+    }
 }
 
-export interface Group extends ContentNode {
-    type: "group";
+export class Group extends ContentNode {
+    type: "group" = "group";
+    constructor(content: Node[], renderInfo?: typeof BaseNode.prototype._renderInfo, position?: typeof BaseNode.prototype.position) {
+        super("group", content, renderInfo, position);
+    }
 }
 
-export interface InlineMath extends ContentNode {
-    type: "inlinemath";
+export class InlineMath extends ContentNode {
+    type: "inlinemath" = "inlinemath";
+    constructor(content: Node[], renderInfo?: typeof BaseNode.prototype._renderInfo, position?: typeof BaseNode.prototype.position) {
+        super("inlinemath", content, renderInfo, position);
+    }
 }
 
-export interface Verb extends BaseNode {
-    type: "verb";
+export class Verb extends BaseNode {
+    type: "verb" = "verb";
     env: string;
     escape: string;
     content: string;
+    constructor(env: string, escape: string, content: string, renderInfo?: typeof BaseNode.prototype._renderInfo, position?: typeof BaseNode.prototype.position) {
+        super("verb", renderInfo, position);
+        this.env = env;
+        this.escape = escape;
+        this.content = content;
+    }
 }
 
-export interface Argument extends ContentNode {
-    type: "argument";
+export class Argument extends ContentNode {
+    type: "argument" = "argument";
     openMark: string;
     closeMark: string;
+    constructor(openMark: string, closeMark: string, content: Node[], renderInfo?: typeof BaseNode.prototype._renderInfo, position?: typeof BaseNode.prototype.position) {
+        super("argument", content, renderInfo, position);
+        this.openMark = openMark;
+        this.closeMark = closeMark;
+    }
 }
 
 export type Node =
