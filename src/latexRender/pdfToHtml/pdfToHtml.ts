e5e2c2153d013deed6e193b2ea36e46372d4beea
kalmanmoshe <moshe196713@gmail.com>
2025-05-08 18:12:02 +0300

    Refactor code structure for improved readability and maintainability; removed redundant code and optimized functions.

--- src/latexRender/pdfToHtml/pdfToHtml.ts
+++ src/latexRender/pdfToHtml/pdfToHtml.ts
@@ -5,13 +5,12 @@ const PdfToCairo = require("./pdftocairo.js")
 import { Md5 } from 'ts-md5';
 
 
-export async function pdfToSVG(pdfData: any, config: { invertColorsInDarkMode: boolean,sourceHash: string }) {
+export async function pdfToSVG(pdfData: Buffer<ArrayBufferLike>, config: { invertColorsInDarkMode: boolean,sourceHash: string }) {
     const hashSVG = (svg: string) => {
         const id = Md5.hashStr(svg.trim()).toString();
         const randomString = Math.random().toString(36).substring(2, 10);
         return id.concat(randomString);
     };
-
     const pdftocairo = await PdfToCairo();
     pdftocairo.FS.writeFile('input.pdf', pdfData);
     pdftocairo._convertPdfToSvg();
