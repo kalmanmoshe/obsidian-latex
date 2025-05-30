e7724bc3c9714cde3164a47ee9178e9e720ba9ed
kalmanmoshe <moshe196713@gmail.com>
2025-05-31 01:32:45 +0300

    Improved catching edge cases

--- src/obsidian/file_watch.ts
+++ src/obsidian/file_watch.ts
@@ -56,7 +56,6 @@ const shouldRefreshFile = (match: { enabled: any; isFile: any; isInFolder: any; 
 	match.enabled && (match.isFile || match.isInFolder);
 
 export const onFileChange = async (plugin: Moshe, file: TAbstractFile) => {
-	console.debug(`File change detected: ${file.path}`);
 	if (!(file instanceof TFile)) return;
 	const fileMatches = Object.values(filePathMatch(plugin, file));
 	if (fileMatches.some(shouldRefreshFile)) {
